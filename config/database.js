5d3169306ad49399278b4ff115e3f81bf69e6817
jigo961257 <mac@MAC-02.local>
2022-11-18 08:58:24 +0530

    update pool file

--- config/database.js
+++ config/database.js
@@ -1,13 +1,23 @@
 const mysql = require("mysql")
 const env = process.env;
 
+// const pool = mysql.createPool({
+//     host: env.DB_HOST,
+//     user: env.DB_USER,
+//     password: env.DB_PASS,
+//     connectionLimit: 10,
+//     database: env.DB_NAME,
+// })
+
 const pool = mysql.createPool({
-    host: env.DB_HOST,
-    user: env.DB_USER,
-    password: env.DB_PASS,
+    host: "containers-us-west-129.railway.app",
+    port: 7370,
+    user: "root",
+    password: "mPnucMn0lFzE5wukBckt",
     connectionLimit: 10,
-    database: env.DB_NAME
+    database: "railway",
 })
+
 
 // var pool = {
 //     "users": [],
