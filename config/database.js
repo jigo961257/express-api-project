273f7dc7e3f88d506708d1f9613d8d12947b0730
jigo961257 <mac@MAC-02.local>
2022-11-18 10:01:58 +0530

    update env-port

--- config/database.js
+++ config/database.js
@@ -1,22 +1,23 @@
 const mysql = require("mysql")
 const env = process.env;
 
-// const pool = mysql.createPool({
-//     host: env.DB_HOST,
-//     user: env.DB_USER,
-//     password: env.DB_PASS,
-//     connectionLimit: 10,
-//     database: env.DB_NAME,
-// })
-
 const pool = mysql.createPool({
-    host: "containers-us-west-129.railway.app",
+    host: env.DB_HOST,
     port: 7370,
-    user: "root",
-    password: "mPnucMn0lFzE5wukBckt",
+    user: env.DB_USER,
+    password: env.DB_PASS,
     connectionLimit: 10,
-    database: "railway",
+    database: env.DB_NAME,
 })
+
+// const pool = mysql.createPool({
+//     host: "containers-us-west-129.railway.app",
+//     port: 7370,
+//     user: "root",
+//     password: "mPnucMn0lFzE5wukBckt",
+//     connectionLimit: 10,
+//     database: "railway",
+// })
 
 
 // var pool = {
